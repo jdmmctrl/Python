7df8572eabb823f47d8525f6bf32e9993d800399
jdmmctrl <juandavidmurillomejia0718@outlook.es>
2023-04-13 23:34:56 -0500

    Actualizacion 13/04/2023/00

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+from time import sleep
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+print("Blast off!! 🚀")
+
+
+countdown = [4, 3, 2, 1, 0]
+
+for number in countdown:
+    print(number)
+    sleep(1)  # Wait 1 second
+print("Blast off!! 🚀")
